3fb73ae1fa538155c12bf2440b0774cb8b3adcfb
Ingrid Rauany <ingrid.rauany@hotmail.com>
2024-06-11 20:01:15 -0300

    Testes de componente cart-item

--- projeto03/components/cart-item.js
+++ projeto03/components/cart-item.js
@@ -1,16 +1,27 @@
-export default function CartItem() {
+import { useState } from "react";
+
+export default function CartItem({ product }) {
+	const [quantity, setQuantity] = useState(1);
+
 	return (
-		<div className="flex justify-between mt-6">
+		<div className="flex justify-between mt-6" data-testid="cart-item">
 			<div className="flex">
 				<img
 					className="h-20 w-20 object-cover rounded"
-					src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
-					alt=""
+					src={product.image}
+					alt={product.title}
+					data-testid="image"
 				/>
 				<div className="mx-3">
-					<h3 className="text-sm text-gray-600">Mac Book Pro</h3>
+					<h3 className="text-sm text-gray-600">{product.title}</h3>
 					<div className="flex items-center mt-2">
-						<button className="text-gray-500 focus:outline-none focus:text-gray-600">
+						<button
+							className="text-gray-500 focus:outline-none focus:text-gray-600"
+							onClick={() =>
+								setQuantity(quantity > 0 ? quantity - 1 : 0)
+							}
+							disabled={quantity == 0}
+						>
 							<svg
 								className="h-5 w-5"
 								fill="none"
@@ -23,8 +34,16 @@ export default function CartItem() {
 								<path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
 							</svg>
 						</button>
-						<span className="text-gray-700 mx-2">2</span>
-						<button className="text-gray-500 focus:outline-none focus:text-gray-600">
+						<span
+							data-testid="quantity"
+							className="text-gray-700 mx-2"
+						>
+							{quantity}
+						</span>
+						<button
+							className="text-gray-500 focus:outline-none focus:text-gray-600"
+							onClick={() => setQuantity(quantity + 1)}
+						>
 							<svg
 								className="h-5 w-5"
 								fill="none"
@@ -40,7 +59,7 @@ export default function CartItem() {
 					</div>
 				</div>
 			</div>
-			<span className="text-gray-600">20$</span>
+			<span className="text-gray-600">${product.price}</span>
 		</div>
 	);
 }
